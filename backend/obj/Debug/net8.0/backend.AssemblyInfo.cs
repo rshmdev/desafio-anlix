cb6b843ba383394f0ee1093042f8a8f2ee38d77a
Rian <rianherminio2002@gmail.com>
2024-06-06 13:55:42 -0300

    feat: gitignore

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee65fac40485b9b4fa8d12a22a228f7a3c376b86")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+938f1eb03e081a3c0bdbd50a7a125102ad668cc5")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
